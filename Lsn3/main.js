a8cd25d8a5734d63ba44096afbe3637b87310151
BiteGigabyte <uyra.p@i.ua>
2023-02-08 14:57:18 +0200

    JS домашнє завдання 3 завершено.
    - готово відсотків 100%.
    - буду дивитись превью до наступного уроку.

--- Lsn3/main.js
+++ Lsn3/main.js
@@ -166,6 +166,7 @@ for(const mass_o of products)
 
 
 
+
 //     є масив
 // let users = [
 //     {name: 'vasya', age: 31, status: false},
@@ -181,7 +182,45 @@ for(const mass_o of products)
 //     {name: 'max', age: 31, status: true}
 // ];
 // за допомоги циклу вивести:
-//     - користувачів зі статусом true
+// - користувачів зі статусом true
 // - користувачів зі статусом false
 // - користувачів які старші за 30 років
-
+//
+//
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+//
+document.write(`<h2>Task_7</h2>`);
+document.write("");
+    document.write(`<h3>status: false;</h3>`);
+for (user of users) {
+    if (user["status"] === false) {
+        document.write(`<h4>${user["name"]}</h4>`);
+    }
+}
+    //
+    document.write(`<h3>status: true;</h3>`);
+    for (user of users) {
+        if (user["status"] === true) {
+            document.write(`<h4>${user["name"]}</h4>`);
+        }
+    }
+    //
+document.write(`<h3>age: over thirty years;</h3>`);
+for (user of users) {
+    if (user["age"] >= 31) {
+        document.write(`<h4>${user["name"]}</h4>`);
+    }
+}
+document.write(`<hr>`);
