6ee3beccc801a8be97c1d2175ba7aa44a6ae9c98
BiteGigabyte <uyra.p@i.ua>
2023-02-12 22:50:31 +0200

    JS домашнє завдання 4 завершено.
    - готово відсотків 53%.
    - роблю далі.

--- Lsn4/main.js
+++ Lsn4/main.js
@@ -1,8 +1,11 @@
+//
+//
+//
 // - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
-document.write(`<div class="bodys">`);
 document.write(`<h2>Task_1<h2>`);
 //
 let a = 4, b = 4;
+
 function rectangle(a, b) {
     return (a * b);
 }
@@ -17,6 +20,7 @@ document.write(`<h3>-------------------</h3>`);
 document.write(`<h2>Task_2<h2>`);
 //
 let r = 5;
+
 function circle(r) {
     let pi = 3.141592;
     return (pi * (r * r));
@@ -33,6 +37,7 @@ document.write(`<h3>-------------------</h3>`);
 document.write(`<h2>Task_3<h2>`);
 //
 let h = 10, r1 = 30;
+
 function cylinder(r1, h) {
     let pi = 3.141592;
     return ((2 * pi * r1 * h) + (2 * pi * (r1 * r1)));
@@ -48,6 +53,7 @@ document.write(`<h3>-------------------</h3>`);
 document.write(`<h2>Task_4<h2>`);
 //
 let arrayOne = [2, 1, 4, 3, 6, 5, 8, 7];
+
 function cout(arrayOne) {
     for (const arrayOneElement of arrayOne) {
         document.write(`${arrayOneElement} `);
@@ -61,6 +67,7 @@ document.write(`<h3>-------------------</h3>`);
 
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
 document.write(`<h2>Task_5<h2>`);
+
 //
 function paragraph(arguments) {
     return `<p>${arguments}</p>`;
@@ -111,16 +118,28 @@ document.write(`<h3>-------------------</h3>`);
 
 document.write(`<h2>Task_8<h2>`);
 //
-
-
-
-
+let arrayA = ['wow', 2, 4, 'Hello', true, [3.2, 2, 3], {step: 1, steptwo: 2}];
+//
+function elements(array) {
+document.write(`<ol>`);
+    for (let f = 0; f < array.length;++f)
+    {
+        document.write(`<li>${array[f]}</li>`);
+    }
+document.write(`</ol>`);
+}
+elements(arrayA);
 document.write(`<h3>-------------------</h3>`);
 
 
 
-
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+
+
+
+
+
 // - створити функцію яка повертає найменьше число з масиву
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
@@ -128,5 +147,3 @@ document.write(`<h3>-------------------</h3>`);
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
 
-
-document.write(`</div>`);
